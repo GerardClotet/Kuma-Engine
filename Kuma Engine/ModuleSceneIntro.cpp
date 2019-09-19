825ef9ac3e1ccefcb6ce8bab845217931937c3cc
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2019-09-19 23:31:08 +0200

    Added test ui

--- Kuma Engine/ModuleSceneIntro.cpp
+++ Kuma Engine/ModuleSceneIntro.cpp
@@ -24,26 +24,14 @@ bool ModuleSceneIntro::Start()
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 
-	//// Setup window
-	//SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	//SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	//SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-	//SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-	//window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-	//SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-	//SDL_GL_MakeCurrent(window, gl_context);
-	//SDL_GL_SetSwapInterval(1); // Enable vsync
-	//
+	
 
 	// Setup Dear ImGui context
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 
-	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-   //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	
 
 	  // Setup Dear ImGui style
 	ImGui::StyleColorsDark();
@@ -106,10 +94,179 @@ update_status ModuleSceneIntro::Update(float dt)
 
 	bool show_another_window = true;
 
-	ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-	ImGui::Text("Hello from another window!");
+	ImGui::Begin("ImGui Test", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+
+	//Begin Menu
+	if (ImGui::BeginMenu("Menu"))
+	{
+		//TODO:/ WE NEED TO PUT HERE THE DIFFERENT OPTIONS OF THE MENU, AS OPEN, SAVE, QUIT...
+		ImGui::EndMenu();
+	}
+
+	if (ImGui::BeginMenu("Examples"))
+	{
+		ImGui::EndMenu();
+	}
+
+	//Text
+	ImGui::Text("ImGui says hello");
+
+	//Headers with options
+	if (ImGui::CollapsingHeader("Help"))
+	{
+
+	}
+
+	if (ImGui::CollapsingHeader("Window options"))
+	{
+
+	}
+
+	if (ImGui::CollapsingHeader("Widgets"))
+	{
+		//Sub menus
+		if (ImGui::TreeNode("Basic"))
+		{
+
+			//button
+			static int clicked = 0;
+			if (ImGui::Button("Button"))
+				clicked++;
+			if (clicked & 1)
+			{
+				ImGui::SameLine();
+				ImGui::Text("Thanks for clicking me!");
+			}
+
+			//checkbox
+			static bool checkBox = true;
+			ImGui::Checkbox("checkbox", &checkBox);
+
+			//circular checkbox
+			static int radio = 0;
+			ImGui::RadioButton("radio a", &radio, 0); ImGui::SameLine();
+			ImGui::RadioButton("radio b", &radio, 1); ImGui::SameLine();
+			ImGui::RadioButton("radio c", &radio, 2);
+
+			//color buttons with text and states
+			for (int i = 0; i < 7; i++)
+			{
+				if (i > 0)
+					ImGui::SameLine();
+				ImGui::PushID(i);
+				ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(i / 7.0f, 0.6f, 0.6f));
+				ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(i / 7.0f, 0.7f, 0.7f));
+				ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(i / 7.0f, 0.8f, 0.8f));
+				ImGui::Button("Click");
+				ImGui::PopStyleColor(3);
+				ImGui::PopID();
+			}
+
+			//Text with tooltips
+			ImGui::Text("Hover over me");
+			if (ImGui::IsItemHovered())
+				ImGui::SetTooltip("I am a tooltip");
+			ImGui::SameLine();
+			ImGui::Text("- or me");
+			if (ImGui::IsItemHovered())
+			{
+				ImGui::BeginTooltip();
+				ImGui::Text("I am a fancy tooltip");
+				static float arr[] = { 0.6f, 0.1f, 1.0f, 0.5f, 0.92f, 0.1f, 0.2f };
+				ImGui::PlotLines("Curve", arr, IM_ARRAYSIZE(arr));
+				ImGui::EndTooltip();
+			}
+
+			//text separator
+			ImGui::Separator();
+
+			ImGui::LabelText("label", "Value");
+
+			//It shows differents options :)
+			{
+				const char* items[] = { "AAAA", "BBBB", "CCCC", "DDDD", "EEEE", "FFFF", "GGGG", "HHHH", "IIII", "JJJJ", "KKKK", "LLLLLLL", "MMMM", "OOOOOOO" };
+				static int item_current = 0;
+				ImGui::Combo("combo", &item_current, items, IM_ARRAYSIZE(items));
+			}
+
+			{
+				//Input text
+				static char str0[128] = "Hello, world!";
+				ImGui::InputText("input text", str0, IM_ARRAYSIZE(str0));
+
+				//Input Int
+				static int i0 = 123;
+				ImGui::InputInt("input int", &i0);
+
+				//Input float
+				static float f0 = 0.001f;
+				ImGui::InputFloat("input float", &f0, 0.01f, 1.0f, "%.3f");
+
+				//3 Input floats
+				static float vec4a[4] = { 0.10f, 0.20f, 0.30f, 0.44f };
+				ImGui::InputFloat3("input float3", vec4a);
+
+				//Drag Int
+				static int i1 = 50, i2 = 42;
+				ImGui::DragInt("drag int", &i1, 1);
+
+				ImGui::DragInt("drag int 0..100", &i2, 1, 0, 100, "%d%%");
+
+				//Drag float
+				static float f1 = 1.00f, f2 = 0.0067f;
+				ImGui::DragFloat("drag float", &f1, 0.005f);
+				ImGui::DragFloat("drag small float", &f2, 0.0001f, 0.0f, 0.0f, "%.06f ns");
+
+				//Slider int
+				static int i3 = 0;
+				ImGui::SliderInt("slider int", &i3, -1, 3);
+
+				//Slider float
+				static float f3 = 0.123f, f4 = 0.0f;
+				ImGui::SliderFloat("slider float", &f1, 0.0f, 1.0f, "ratio = %.3f");
+				ImGui::SliderFloat("slider log float", &f2, -10.0f, 10.0f, "%.4f", 2.0f);
+
+				//Slider angle
+				static float angle = 0.0f;
+				ImGui::SliderAngle("slider angle", &angle);
+
+				//Color editor
+				static float col1[3] = { 1.0f,0.0f,0.2f };
+				static float col2[4] = { 0.4f,0.7f,0.0f,0.5f };
+				ImGui::ColorEdit3("color 1", col1);
+				ImGui::ColorEdit4("color 2", col2);
+			}
+
+
+
+			ImGui::TreePop();
+		}
+
+
+		if(ImGui::TreeNode("Tree"))
+		{
+			ImGui::TreePop();
+		}
+		
+		if(ImGui::TreeNode("Bullets"))
+		{
+			ImGui::TreePop();
+		}
+		ImGui::TreeNode("Colored Text");
+		ImGui::TreeNode("Word Wrapping");
+		ImGui::TreeNode("UTF-8 Text");
+		ImGui::TreeNode("Images");
+		ImGui::TreeNode("Selectables");
+		ImGui::TreeNode("Filtered Text Input");
+		ImGui::TreeNode("Multi-line Text Input");
+
+	}
+
+	//Button
 	if (ImGui::Button("Close Me"))
 		show_another_window = false;
+
+
 	ImGui::End();
 
 	ImGui::Render();
