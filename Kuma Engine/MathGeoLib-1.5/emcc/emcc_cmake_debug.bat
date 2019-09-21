69859397b23dd0ba402cbbc26470bbe46ed76633
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2019-09-21 19:43:44 +0200

    files changed

--- Kuma Engine/MathGeoLib-1.5/emcc/emcc_cmake_debug.bat
+++
@@ -1,2 +0,0 @@
-cmake -DCMAKE_TOOLCHAIN_FILE=emcc_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" ..
-pause
--- Kuma Engine/MathGeoLib-1.5/emcc/emcc_cmake_release.bat
+++
@@ -1,2 +0,0 @@
-cmake -DCMAKE_TOOLCHAIN_FILE=emcc_toolchain.cmake -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" ..
-pause
