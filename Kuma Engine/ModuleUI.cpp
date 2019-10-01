c1a1bd049de9b724ba1c858b6aad5a67309103dc
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2019-10-01 17:55:18 +0200

    Update ModuleUI.cpp

--- Kuma Engine/ModuleUI.cpp
+++ Kuma Engine/ModuleUI.cpp
@@ -53,6 +53,11 @@ bool ModuleEditor::Start()
 		LOG("%s", logs);
 	}
 
+	LOG("Vendor: %s", glGetString(GL_VENDOR));
+	LOG("Renderer: %s", glGetString(GL_RENDERER));
+	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
+	LOG("GLSL: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
 	return true;
 
 }
