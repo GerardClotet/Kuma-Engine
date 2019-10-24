6fdab403b284b5ff76d4e55320ed292fe9b0f908
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2019-10-24 22:53:13 +0200

    fixed omega bug with loading default config

--- Kuma Engine/PanelConfig.cpp
+++ Kuma Engine/PanelConfig.cpp
@@ -30,6 +30,10 @@ void PanelConfig::DisplayConfig()
 		if (ImGui::MenuItem("Set Defaults"))
 		{
 			JSON_Object* config_default = App->LoadJSONFile("config.json");
+			if (config_default == nullptr)
+			{
+				config_default = App->LoadJSONFile("Configuration/config.json");
+			}
 			std::list<Module*>::iterator item = App->list_modules.begin();
 			while (item != App->list_modules.end())
 			{
