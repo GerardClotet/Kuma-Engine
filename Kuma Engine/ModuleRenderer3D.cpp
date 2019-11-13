f2be0da26dbabb6b07db5b91a6ddb0265f672903
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2019-11-13 23:54:16 +0100

    printing with light bro

--- Kuma Engine/ModuleRenderer3D.cpp
+++ Kuma Engine/ModuleRenderer3D.cpp
@@ -27,7 +27,7 @@ bool ModuleRenderer3D::Init()
 {
 	LOG("Creating 3D Renderer context");
 	bool ret = true;
-	
+
 
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
@@ -45,23 +45,16 @@ bool ModuleRenderer3D::Init()
 	}
 	else App->saveLog("OpenGL context created");
 	GLenum error = glewInit();
-
-	GameObject* camera_go = App->scene_intro->CreateGameObject(nullptr, OBJECT_TYPE::NONE, "Camera Fake");
-	App->camera->camera_fake = new Component_Camera(camera_go);
-	App->camera->camera_fake->frustum.farPlaneDistance = 1000.0F;
-	actual_camera = App->camera->camera_fake;
-
-
 	if (error != GL_NO_ERROR)
 	{
 		LOG("Error initializing glew library! %s\n", SDL_GetError());
 		ret = false;
 	}
 	else App->saveLog("Initialized glew lib");
-	if(ret == true)
+	if (ret == true)
 	{
 		//Use Vsync
-		if(VSYNC && SDL_GL_SetSwapInterval(1) < 0)
+		if (VSYNC && SDL_GL_SetSwapInterval(1) < 0)
 			LOG("Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
 
 		//Initialize Projection Matrix
@@ -70,7 +63,7 @@ bool ModuleRenderer3D::Init()
 
 		////Check for error
 		GLenum error = glGetError();
-		if(error != GL_NO_ERROR)
+		if (error != GL_NO_ERROR)
 		{
 			LOG("Error initializing OpenGL! %s\n", gluErrorString(error));
 			App->saveLog("Error initializing OpenGL! %s\n", gluErrorString(error));
@@ -81,24 +74,22 @@ bool ModuleRenderer3D::Init()
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 
-	
-		
+
+
 		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 		glClearDepth(1.0f);
 
 		//Initialize clear color
-		glClearColor(actual_camera->color_camera_bg.r, actual_camera->color_camera_bg.g, actual_camera->color_camera_bg.b, actual_camera->color_camera_bg.a);
-
-		//Check for error
-		error = glGetError();
-		if (error != GL_NO_ERROR)
-		{
-			LOG("Error initializing OpenGL! %s\n", gluErrorString(error));
-			ret = false;
-		}
+		glClearColor(0.0f, 0.0f, 0.0f, 1.f);
 
 		GLfloat LightModelAmbient[] = { 0.0f, 0.0f, 0.0f, 1.0f };
 		glLightModelfv(GL_LIGHT_MODEL_AMBIENT, LightModelAmbient);
+
+		lights[0].ref = GL_LIGHT0;
+		lights[0].ambient.Set(0.25f, 0.25f, 0.25f, 1.0f);
+		lights[0].diffuse.Set(0.75f, 0.75f, 0.75f, 1.0f);
+		lights[0].SetPos(0.0f, 0.0f, 2.5f);
+		lights[0].Init();
 
 		GLfloat MaterialAmbient[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, MaterialAmbient);
@@ -106,21 +97,15 @@ bool ModuleRenderer3D::Init()
 		GLfloat MaterialDiffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 		glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, MaterialDiffuse);
 
-
-		glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
-		glClearDepth(1.0f);
-		glClearColor(actual_camera->color_camera_bg.r, actual_camera->color_camera_bg.g, actual_camera->color_camera_bg.b, actual_camera->color_camera_bg.a);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_CULL_FACE);
+		lights[0].Active(true);
 		glEnable(GL_LIGHTING);
 		glEnable(GL_COLOR_MATERIAL);
-		glEnable(GL_TEXTURE_2D);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glEnable(GL_BLEND);
 
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		
+		GameObject* go = App->scene_intro->CreateGameObject(nullptr, OBJECT_TYPE::NONE, "Camera");
+		App->camera->camera_fake = new Component_Camera(go);
+		actual_camera = App->camera->camera_fake;
 	}
 
 	// Projection matrix for
@@ -128,7 +113,7 @@ bool ModuleRenderer3D::Init()
 
 	/*App->hardware->gpu_vendor = (const char*)glGetString(GL_VENDOR);
 	App->hardware->gpu_brand = (const char*)glGetString(GL_RENDERER);*/
-	
+
 	LOG("Vendor: %s", glGetString(GL_VENDOR));
 	LOG("Renderer: %s", glGetString(GL_RENDERER));
 	LOG("OpenGL version supported %s", glGetString(GL_VERSION));
@@ -151,12 +136,12 @@ update_status ModuleRenderer3D::PreUpdate(float dt)
 	glLoadIdentity();
 
 	glMatrixMode(GL_MODELVIEW);
-	glLoadMatrixf(App->camera->camera_fake->GetViewMatrix());
+	glLoadMatrixf(actual_camera->GetViewMatrix());
 
 	// light 0 on cam pos
-	lights[0].SetPos(App->camera->Position.x, App->camera->Position.y, App->camera->Position.z);
+	lights[0].SetPos(actual_camera->frustum.pos.x, actual_camera->frustum.pos.x, actual_camera->frustum.pos.x);
 
-	for(uint i = 0; i < MAX_LIGHTS; ++i)
+	for (uint i = 0; i < MAX_LIGHTS; ++i)
 		lights[i].Render();
 
 	return UPDATE_CONTINUE;
@@ -187,7 +172,7 @@ void ModuleRenderer3D::OnResize(int width, int height)
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	ProjectionMatrix = perspective(60.0f, (float)width / (float)height, 0.125f, 512.0f);
-	glLoadMatrixf(App->camera->camera_fake->GetProjectionMatrix());
+	glLoadMatrixf(actual_camera->GetProjectionMatrix());
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
